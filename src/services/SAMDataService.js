65ae15d5ac25e3de7518138ec47b4ab7b2a9375f
Yichen Tang <ytan415@aucklanduni.ac.nz>
2022-12-23 20:32:18 +1300

    Implement vocalisation list initialisation

---
+++ src/services/SAMDataService.js
@@ -0,0 +1,26 @@
+import store from "@/store";
+
+class SAMDataService {
+
+    setup() {
+        this.initVocalisationList();
+    }
+
+    initVocalisationList() {
+        let req = require.context('@/assets/vocs', false, /^.*\.wav$/);
+        var vocList = req.keys().slice();
+        // Shuffle the list
+        for (let i = vocList.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [vocList[i], vocList[j]] = [vocList[j], vocList[i]];
+        }
+        // Remove prefix "./"
+        vocList.forEach((voc, idx) => {
+            vocList[idx] = voc.slice(2);
+        });
+        store.commit("updateVocalisationList", vocList);
+    }
+
+}
+
+export default new SAMDataService();
