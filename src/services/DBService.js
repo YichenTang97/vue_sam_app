aa0733b8d065a73b85f94ec3e2d7a0e37effc481
Yichen Tang <ytan415@aucklanduni.ac.nz>
2022-12-24 21:00:27 +1300

    Remove single rating database update - redundant
    Save firebase usage

--- src/services/DBService.js
+++ src/services/DBService.js
@@ -33,21 +33,8 @@ class FirestoreDB {
         this.storage = storage;
     }
 
-    async newRating(speaker, emotion, target, type, rating) {
-        try {
-            const docRef = await addDoc(collection(this.db, `users/${store.state.user.uid}/ratings`), {
-                speaker: speaker,
-                emotion: emotion,
-                target: target,
-                type: type,
-                rating: rating,
-                time: serverTimestamp()
-            });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
-    }
+    // eslint-disable-next-line no-unused-vars 
+    newRating(speaker, emotion, target, type, rating) { }
 
     save() {
         return addDoc(collection(this.db, `users/${store.state.user.uid}/final_ratings`), {
